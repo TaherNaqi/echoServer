e60b1f0fc5e2214c247ae688fc9f7e83f4c58c89
TaherNaqi <eng-naqi@hotmail.com>
2022-04-19 21:26:17 +0300

    made path /healthcheck/echo

--- v0.0.0/server.js
+++ v0.0.0/server.js
@@ -4,7 +4,7 @@ const port = 3000;
 const host = "localhost";
 const server = http.createServer(function (req, res) {
   const parsedURL = url.parse(req.url, true);
-  if (parsedURL.pathname == "/echo" && parsedURL.query.message) {
+  if (parsedURL.pathname == "/healthcheck/echo" && parsedURL.query.message) {
     res.end(parsedURL.query.message);
   } else {
     res.end("Request not valid");
