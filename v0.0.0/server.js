a70127b11eff24b522e2a08ce9a854b61944dadb
TaherNaqi <eng-naqi@hotmail.com>
2022-04-19 21:10:29 +0300

    echo server running

---
+++ v0.0.0/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const url = require("url");
+const port = 3000;
+const host = "localhost";
+const server = http.createServer(function (req, res) {
+  const parsedURL = url.parse(req.url, true);
+  if (parsedURL.pathname == "/echo" && parsedURL.query.message) {
+    res.end(parsedURL.query.message);
+  } else {
+    res.end("Request not valid");
+  }
+});
+
+server.listen(port, host, function () {
+  console.log("Echo server is running on port 3000");
+});
